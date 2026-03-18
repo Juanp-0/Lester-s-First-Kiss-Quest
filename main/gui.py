e2e5e788a8a038426b4cc10d8bbc1fe9d709d0c3
Juanp-0 <azooooosito@gmail.com>
2026-03-18 14:52:25 -0600

    Implementacion de GUI WIP

---
+++ main/gui.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+app = tk.Tk()
+app.title("Lester's First Kiss Quest")
+app.geometry("400x300")
+
+etiqueta = tk.Label(app, text="¡Bienvenido a Lester's First Kiss Quest!")
+etiqueta.pack(pady=20)
+
+app.mainloop()
